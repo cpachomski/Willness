fa5b426d0c7f787a036a048e3f268611db28c8cb
cpachomski <cpachomski@gmail.com>
2015-04-27 18:45:07 -0400

    create landing controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      redirect_to :controller => 'home', action: => 'index'
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
