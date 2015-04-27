39ced496cb36fe14d39526836236b713d4dfbd5b
cpachomski <cpachomski@gmail.com>
2015-04-27 18:59:44 -0400

    reincarnate db

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,11 @@
 ActiveRecord::Schema.define(version: 20150427222747) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
