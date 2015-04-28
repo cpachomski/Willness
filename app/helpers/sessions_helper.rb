1ce87b085cbc84cb59ea8c18b694026058e0fafb
cpachomski <cpachomski@gmail.com>
2015-04-28 14:08:46 -0400

    add comments and current_user helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,13 @@
 module SessionsHelper
 
+  #adds user id to session when create action is hit
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  #creates current user from session id if not present
+  def current_user
+    @current_user || = User.find_by(id: session[:user:id])
   end
 
 end
