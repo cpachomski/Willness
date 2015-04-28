700e1b46807249263783bf95e8674bdd7cc47fe0
cpachomski <cpachomski@gmail.com>
2015-04-28 14:56:29 -0400

    add remember helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # creates remember_digest for user and saves it in cookies with hashed user_id for 20 years!!!!!!!!
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   #removes user id from session and removes current user
   def log_out
     session.delete(:user_id)
