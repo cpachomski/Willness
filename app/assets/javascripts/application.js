6e44e98cbf232049a3c2cf094fea29e397834fc6
cpachomski <cpachomski@gmail.com>
2015-04-27 20:22:21 -0400

    setup styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+$document.ready(function(){
+  //sets landing main box to window size
+  var windowHeight = $( window ).height();
+  $('.landing-main').css("height", windowHeight + "px")
+})
