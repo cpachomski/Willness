1f0b8629ecebd8be7425e4396ed67a10812e971e
cpachomski <cpachomski@gmail.com>
2015-04-28 14:25:17 -0400

    add log_in to create action upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      #create session and log_in when user signs up
+      log_in(@user)
       redirect_to @user
     else
       render 'new'
