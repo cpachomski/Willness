7a1008838ee49d18e83019d6512c454033eb6121
cpachomski <cpachomski@gmail.com>
2015-04-28 14:02:58 -0400

    add create action for sessioning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = user.find_by(:email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log user in and redirect to show page
+    else
+      #render error messages
+      render 'new'
+    end
+  end
+
 end
