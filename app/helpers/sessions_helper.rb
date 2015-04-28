c54e785610ae4e3855e65aab24ecb47be27dfd4b
cpachomski <cpachomski@gmail.com>
2015-04-28 14:23:39 -0400

    add session logout helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,14 +5,22 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #removes user id from session and removes current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   #creates current user from session id if not present
   def current_user
-    @current_user || = User.find_by(id: session[:user:id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   #returns true if user is logged in
-  def logged_in
+  def logged_in?
     !current_user.nil?
   end
+
+
 
 end
