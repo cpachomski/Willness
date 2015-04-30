ead0eadb31a1fb65bb967ce16ac9d47627ba3ec9
cpachomski <cpachomski@gmail.com>
2015-04-30 13:44:05 -0400

    add task_params method and new/create actions"

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,20 @@
 class TasksController < ApplicationController
+
   def new
+    @task = Task.new
   end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:user_id, :title, :description, :value, :category)
+  end
+
 end
