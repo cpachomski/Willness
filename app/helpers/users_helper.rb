5bcad8f5db87260a66cd117b68efbf7ca5c89387
cpachomski <cpachomski@gmail.com>
2015-05-02 16:16:21 -0400

    add setup_client method to users to be run upon login"

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,40 @@
 module UsersHelper
 
 
+  def setup_client
+    consumer_key = Rails.application.secrets.fitbit_key
+    consumer_secret = Rails.application.secrets.fitbit_secret
+
+    client = Fitgem::Client.new({:consumer_key => consumer_key, :consumer_secret => consumer_secret})
+    request_token = client.request_token
+    token = request_token.token
+    secret = request_token.secret
+
+
+
+    access_token = client.authorize(token, secret, {:oauth_verifier => verifier})
+
+    puts "verifier = " + verifier
+    puts "token = " + access_token.token
+    puts "secret =" + access_token.secret
+
+    return {:token => access_token.token, :secret => access_token.secret}
+  end
+
+  def user_steps
+
+     setup_client[:token]
+
+  end
+
+
+
 
   # 1. create a current timestamsp
   # 2.  query the db for the current day for current user
   # 3.  query for the the steps in same query
   # 4.  set return daily steps variable
   # 5.  add to users show action
-
 
   def current_daily_steps
 
