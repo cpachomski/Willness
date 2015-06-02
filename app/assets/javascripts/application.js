e37c38baf2a0e5795119d609a1f89a136270eb7e
cpachomski <cpachomski@gmail.com>
2015-06-02 14:23:45 -0400

    connect login correctly and add switch to register panel and back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,30 +27,30 @@ $( document ).ready(function(){
   var windowHeight = $( window ).height();
   $('.landing-main').css("height", windowHeight + "px");
 
+  $(function() {
+
+    $('#login-form-link').click(function(e) {
+        $(".login-box").delay(100).fadeIn(100);
+        $("#register-box").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+        $("#register-box").delay(100).fadeIn(100);
+        $(".login-box").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
+  });
 
 });
 
 
   //sets transition for login/register box
 
-$(function() {
-
-  $('#login-form-link').click(function(e) {
-      $("#login-form").delay(100).fadeIn(100);
-      $("#register-form").fadeOut(100);
-      $('#register-form-link').removeClass('active');
-      $(this).addClass('active');
-      e.preventDefault();
-  });
-  $('#register-form-link').click(function(e) {
-      $("#register-form").delay(100).fadeIn(100);
-      $("#login-form").fadeOut(100);
-      $('#login-form-link').removeClass('active');
-      $(this).addClass('active');
-      e.preventDefault();
-  });
-
-});
 
 
 
