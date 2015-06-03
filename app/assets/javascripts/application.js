130ff97c562a8bce0c6b283756a306475396ee3e
cpachomski <cpachomski@gmail.com>
2015-06-03 04:45:02 -0400

    add dropdown to title bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+
+
 //= require bootstrap
 //= require bootstrap-sprockets
-//= require bootstrap-datepicker
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 //= require_self
