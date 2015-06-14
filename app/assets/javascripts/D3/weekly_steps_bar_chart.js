421d5d8a23709f1cb5403a1677e1b4d585d659ba
cpachomski <cpachomski@gmail.com>
2015-06-13 22:03:50 -0400

    begin adding daily tasks and add to day partial view on dashboard"

--- app/assets/javascripts/D3/weekly_steps_bar_chart.js
+++ app/assets/javascripts/D3/weekly_steps_bar_chart.js
@@ -1,6 +1,8 @@
+
+
 var margin = {top:40, right:20, bottom:30, left:40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
 
 
@@ -13,6 +15,46 @@ var y = d3.scale.linear()
           .range([height,0]);
 //set axes
 var XAxis = d3.svg.axis().scale(x).orient("bottom");
-var YAxis = d3.svg.axis().scale(y).orient('left').tickFormat(formatPercent);
+var YAxis = d3.svg.axis().scale(y).orient('left');
+
+
+
+var svg = d3.select(".weekly-steps").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform','translate'+margin.left+ ","+margin.top+"_)");
+
+
+d3.tsv("weekly_steps.tsv", type, function(error, data){
+  x.domain(data.map(function(d){ return d.day;}));
+  y.domain([0, d3.max(data, function(d){return d.steps;})]);
+
+  svg.append('g')
+    .attr('class', 'y axis')
+    .call(YAxis)
+    .append('text')
+    .attr("transform", "rotate(-90)")
+    .attr('dy', ".71em")
+    .style('text-anchor', 'end')
+    .text('Steps');
+
+  svg.selectAll('.bar')
+     .data(data)
+     .enter().append('rect')
+     .attr('class', 'bar')
+     .attr('x', function(d) { return x(d.day); })
+     .attr('width', x.rangeBand())
+     .attr('y', function(d) { return y(d.steps); })
+     .attr('height', function(d) {return height - y(d.frequency); })
+
+});
+
+function type(d) {
+  d.steps = +d.steps;
+  return d;
+}
+
+
 
 
